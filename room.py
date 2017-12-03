892464a75eef825475123dc046da10e9dc24380a
jmpietrz <jmpietrz@mtu.edu>
2017-12-03 01:55:07 +0000

    Changed main semantics, added performAction

--- room.py
+++ room.py
@@ -2,6 +2,8 @@
 
 import string
 
+import textCommand
+
 class Room:
 	def __init__(self, items, id, description, up, right, down, left, objects):
 		self.id = id
@@ -12,9 +14,9 @@ class Room:
 
 		self.items = []
 
-		for i in items:
-			for j in objects:
-				if i.id == j and j not in self.items:
+		for j in objects:
+			for i in items:
+				if i.id == j and i not in self.items:
 					self.items.append(i)
 
 	def goDirection(self, direction):
@@ -32,8 +34,8 @@ class Room:
 		return self.adjoiningRooms[index]
 
 class Item:
-	def __init__(self, id, name, description):
-		self.id = id
+	def __init__(self, itemId, name, description):
+		self.id = itemId
 		self.name = name
 		self.description = description
 
@@ -44,7 +46,8 @@ class Map:
 		itemsContents = itemsFile.readlines()
 		items = []
 		for i in itemsContents:
-			items.append(Item(i[0], i[1], i[2]))
+			j = i.split(",,")
+			items.append(Item(j[0], j[1], j[2]))
 
 		roomsFile = open(roomsFileName, "r")
 		roomsContent = roomsFile.readlines()
@@ -52,15 +55,24 @@ class Map:
 		for i in roomsContent:
 			roomsText = i.split(",,")
 			self.rooms.append(Room(items, roomsText[0], roomsText[1], roomsText[2], roomsText[3],
-				roomsText[4], roomsText[5], roomsText[6]))
+				roomsText[4], roomsText[5], roomsText[6].split(",")))
 
-if __name__ == "__main__":
+	def performAction(action):
+		if action.type == "move":
+			newRoom = self.rooms[self.position].goDirection(action.object)
+			if (newRoom < 0):
+				return "Cannot go " + action.object + "."
+			self.position = newRoom
+			return self.rooms[self.position].description
+
+
+def main():
     maps = Map("rooms.txt", "items.txt")
     for i in maps.rooms:
-    	print(i.description)
+    	print(i.id + ": " + i.description)
     print("Going up in room 1: " + str(maps.rooms[0].goDirection("north")))
     print("Going right in room 1: " + str(maps.rooms[0].goDirection("east")))
     print("Going down in room 1: " + str(maps.rooms[0].goDirection("south")))
     print("Going left in room 1: " + str(maps.rooms[0].goDirection("west")))
-    for i in maps.rooms[0].adjoiningRooms:
-    	print(str(i))
+
+if __name__ == "__main__": main()
