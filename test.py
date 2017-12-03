95b5561bb4ece5fe45c2c1812d7ba7fec4dd7b1e
Nicholas Hoffbeck <hoffbecks@gmail.com>
2017-12-02 19:46:36 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
-
-# Your Account SID from twilio.com/console
-account_sid = "AC7fd1294880ac928421a5823df2c4e2d6"
-# Your Auth Token from twilio.com/console
-auth_token  = "08b348a6ef5d67db668511770d91f563"
-
-client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
@@ -17,8 +9,10 @@ def sms_reply():
     # Start our TwiML response
     resp = MessagingResponse()
 
+    returnText = someFunction()
+
     # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
+    resp.message(returnText)
 
     return str(resp)
 
