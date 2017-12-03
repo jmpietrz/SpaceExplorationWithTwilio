abb900925edd70bd9ebf5aabe88f518e4a082aeb
jmpietrz <jmpietrz@mtu.edu>
2017-12-03 02:23:34 +0000

    "fixed" the spaces

--- test.py
+++ test.py
@@ -29,10 +29,7 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
     resp = MessagingResponse()
-
-	# Your Account SID from twilio.com/console
 	account_sid = "AC7fd1294880ac928421a5823df2c4e2d6"
-	# Your Auth Token from twilio.com/console
 	auth_token = "08b348a6ef5d67db668511770d91f563"
 	client = Client(account_sid, auth_token)
 	#message = client.messages.create(to="+16514916260",
